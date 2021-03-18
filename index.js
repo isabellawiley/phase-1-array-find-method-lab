6e2e4b93d62c98e652f1a5e492d22f405438521b
Isabella <wileyisabella@gmail.com>
2021-03-18 15:25:32 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 
+function superbowlWin(record){
+    const game = record.find(( {result}) => result === "W")
+    if (game){
+        let win = record.indexOf(game)
+        let num = record[win]["year"]
+        return num
+    }
+    else{
+        return game
+    }
+    
+}
